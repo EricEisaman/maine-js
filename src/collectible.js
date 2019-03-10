64b96c1086af71914cca10acde5332b5016321c3
Glitch (maine-js) <none>
2019-03-10 09:20:48 +0000

    update collectible threshold

--- src/collectible.js
+++ src/collectible.js
@@ -1,6 +1,6 @@
 export default CS1=>{AFRAME.registerComponent("collectible", {
 	schema: {
-    threshold: {type: 'number', default: 2.0},
+    threshold: {type: 'number', default: 2.7},
     soundCollect: {type: 'string',default:'https://cdn.glitch.com/f8abb766-9950-44ff-9adb-2f5f53fdaf1b%2Fpowerup_1.mp3?1552158629039'},
     soundLoop: {},
     cb:{type:'string',default:''}
@@ -22,6 +22,7 @@ export default CS1=>{AFRAME.registerComponent("collectible", {
       let collectedEntity = CS1.collectibles[data.index];
       if(collectedEntity.el.components.sound__loop)collectedEntity.el.components.sound__loop.pause();
       collectedEntity.el.setAttribute('visible',false);
+      collectedEntity.el.setAttribute('scale','0 0 0');
       collectedEntity.soundIsPlaying=true;
       collectedEntity.el.components.sound__collect.playSound();
       if(collectedEntity.data.cb){
@@ -49,6 +50,7 @@ export default CS1=>{AFRAME.registerComponent("collectible", {
     if(CS1.socket.disconnected){
       if(this.el.components.sound__loop)this.el.components.sound__loop.pause();
       this.el.setAttribute('visible',false);
+      this.el.setAttribute('scale','0 0 0');
       this.soundIsPlaying=true;
       this.el.components.sound__collect.playSound();
       if(this.data.cb)CS1.game[this.data.cb](this.el);
